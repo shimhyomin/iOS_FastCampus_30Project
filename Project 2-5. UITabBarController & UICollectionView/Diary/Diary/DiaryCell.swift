e0ca2a7b4fe5327a283ae03aece02212711b1177
shimhyomin <shimhm03@gmail.com>
2021-10-03 15:01:27 +0900

    Add Project 2-5 UITabBarController & UICollectionView Setting

---
+++ Project 2-5. UITabBarController & UICollectionView/Diary/Diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  Diary
+//
+//  Created by shm on 2021/10/03.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ Project 2-5. UITabBarController & UICollectionView/Diary/Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by shm on 2021/10/03.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
